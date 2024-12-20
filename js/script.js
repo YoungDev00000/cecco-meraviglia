b7c2da429641210a2402557df12daa65e55fbc26
zanottor <zanottor@previdom.previnet.it>
2024-12-20 16:31:42 +0100

    nuova interfaccia con webGL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Crea la scena
+    const scene = new THREE.Scene();
+
+    // Crea la camera
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Crea il renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Aggiungi una luce alla scena
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    // Crea la figura 3D (es. un cubo)
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    document.getElementById('index-button').addEventListener('click', () => {
+        camera.position.x = 0;
+        camera.position.y = 0;
+    });
+    document.getElementById('projects-button').addEventListener('click', () => {
+        camera.position.x = 2;
+        camera.position.y = 0;
+    });
+    document.getElementById('contact-button').addEventListener('click', () => {
+        camera.position.x = -2;
+        camera.position.y = 0;
+    });
+
+    // Funzione di animazione
+    function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+
+        //console.log('animate');
+
+    }
+
+    animate();
+});
+
 function showLargeImage(img) {
     const largeImageContainer = document.createElement('div');
     largeImageContainer.className = 'large-image';
