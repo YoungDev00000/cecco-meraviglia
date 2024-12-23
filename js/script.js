6c8274d332c7f9facf909abb4a1362fa3509ccea
unknown <zanottoraimondo@gmail.it>
2024-12-23 17:39:01 +0100

    nuova interfaccia project

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Crea la camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.z = 30;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xffffff, 1); // Imposta il background bianco
     document.body.appendChild(renderer.domElement);
 
     const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -22,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         function animate() {
             requestAnimationFrame(animate);
             model.rotation.x += 0.005; // Ruota il modello
-            model.rotation.y += 0.005; // Ruota il modello
+            model.rotation.y -= 0.005; // Ruota il modello
             renderer.render(scene, camera);
         }
         
@@ -31,21 +32,31 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error(error);
     });
 
-    function loadContent(file) {
-        fetch(file)
+    function loadContent(url, callback = () => {}) {
+        fetch(url)
             .then(response => response.text())
-            .then(data => {
-                document.getElementById('content').innerHTML = data;
+            .then(html => {
+                document.getElementById('content').innerHTML = html;
+                callback();
             })
-            .catch(error => console.error('Error loading content:', error));
+            .catch(error => {
+                console.error('Error loading content:', error);
+            });
     }
 
+    
     document.getElementById('index-button').addEventListener('click', () => {
         loadContent('home.html');
     });
+
     document.getElementById('projects-button').addEventListener('click', () => {
-        loadContent('projects.html');
+        loadContent('projects.html', function() {
+            showList('biasia');
+            showList('vemecell');
+            showList('tm8');
+            });
     });
+
     document.getElementById('contact-button').addEventListener('click', () => {
         loadContent('contact.html');
     });
@@ -66,4 +77,19 @@ function showLargeImage(img) {
     largeImageContainer.appendChild(largeImage);
     largeImageContainer.appendChild(closeButton);
     document.body.appendChild(largeImageContainer);
+}
+
+function showList(brand) {
+    document.getElementById(brand).addEventListener('click', function(event) {
+        event.preventDefault();
+        hideAllLists();
+        document.getElementById(brand + '-images').style.display = 'block';
+    });
+}
+
+function hideAllLists() {
+    const lists = document.querySelectorAll('.image-list');
+    lists.forEach(list => {
+        list.style.display = 'none';
+    });
 }
