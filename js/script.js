0818f47413504e2b30ec958ef05bfb6fbbc8a074
unknown <zanottoraimondo@gmail.it>
2024-12-29 12:11:57 +0100

    new 3d model + interaction

--- js/script.js
+++ js/script.js
@@ -4,26 +4,102 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Crea la camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 30;
+    camera.position.z = 3;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xffffff, 1); // Imposta il background bianco
+    renderer.setClearColor(0xDFDFDF, 1); // Imposta il background bianco
     document.body.appendChild(renderer.domElement);
 
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
+    // Aggiungi luce direzionale
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
+
+    // Aggiungi luce ambientale per ridurre il contrasto
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Colore bianco, intensità 0.5
+    scene.add(ambientLight);
 
     const loader = new THREE.GLTFLoader();
-    loader.load('3d/edificio.glb', function (gltf) {
+    loader.load('3d/cranio.glb', function (gltf) {
         const model = gltf.scene;
         scene.add(model);
 
+        let isDragging = false;
+        let previousMousePosition = {
+            x: 0,
+            y: 0
+        };
+
+        function onMouseDown(e) {
+            isDragging = true;
+        }
+
+        function onMouseMove(e) {
+            if (isDragging) {
+                const deltaMove = {
+                    x: e.offsetX - previousMousePosition.x,
+                    y: e.offsetY - previousMousePosition.y
+                };
+
+                model.rotation.y += deltaMove.x * 0.005;
+                model.rotation.x += deltaMove.y * 0.005;
+            }
+
+            previousMousePosition = {
+                x: e.offsetX,
+                y: e.offsetY
+            };
+        }
+
+        function onMouseUp(e) {
+            isDragging = false;
+        }
+
+        function onTouchStart(e) {
+            isDragging = true;
+            previousMousePosition = {
+                x: e.touches[0].clientX,
+                y: e.touches[0].clientY
+            };
+        }
+
+        function onTouchMove(e) {
+            if (isDragging) {
+                const deltaMove = {
+                    x: e.touches[0].clientX - previousMousePosition.x,
+                    y: e.touches[0].clientY - previousMousePosition.y
+                };
+
+                model.rotation.y += deltaMove.x * 0.005;
+                model.rotation.x += deltaMove.y * 0.005;
+
+                previousMousePosition = {
+                    x: e.touches[0].clientX,
+                    y: e.touches[0].clientY
+                };
+            }
+        }
+
+        function onTouchEnd(e) {
+            isDragging = false;
+        }
+
+        renderer.domElement.addEventListener('mousedown', onMouseDown);
+        renderer.domElement.addEventListener('mousemove', onMouseMove);
+        renderer.domElement.addEventListener('mouseup', onMouseUp);
+        renderer.domElement.addEventListener('mouseleave', onMouseUp);
+
+        renderer.domElement.addEventListener('touchstart', onTouchStart);
+        renderer.domElement.addEventListener('touchmove', onTouchMove);
+        renderer.domElement.addEventListener('touchend', onTouchEnd);
+
         function animate() {
             requestAnimationFrame(animate);
-            model.rotation.x += 0.005; // Ruota il modello
-            model.rotation.y -= 0.005; // Ruota il modello
+            if (!isDragging) {
+                model.rotation.x += 0.005; // Ruota il modello
+                model.rotation.y -= 0.005; // Ruota il modello
+            }
             renderer.render(scene, camera);
         }
         
@@ -54,6 +130,7 @@ document.addEventListener('DOMContentLoaded', function () {
             showList('biasia');
             showList('vemecell');
             showList('tm8');
+            showList('maxMara');
             });
     });
 
@@ -93,3 +170,9 @@ function hideAllLists() {
         list.style.display = 'none';
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.getElementById('flash-text').style.display = 'none';
+    }, 7000);
+});
